1f04c9a531923b0ad96b2a12387bfd67a4cbb622
dev-Leirbag <gabrielaf2186@gmail.com>
2026-04-17 00:57:01 -0300

    First commit

---
+++ src/test/java/com/api/feedbackSentinel/FeedbackSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.feedbackSentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackSentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
