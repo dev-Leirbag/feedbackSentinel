a690dd6f8700a4dee3366e3557885b0a0322afd5
dev-Leirbag <gabrielaf2186@gmail.com>
2026-04-18 02:11:15 -0300

    fix: ajusta mapeamento dos DTOs de resposta do GPT

--- src/main/java/integration/openai/dto/GptResponseDTO.java
+++ src/main/java/integration/openai/dto/GptResponseDTO.java
@@ -1,17 +1,22 @@
 package integration.openai.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GptResponseDTO {
     List<ChoiceDTO> choices;
 
 
     public class ChoiceDTO {
         MessageDTO message;
-    }
-
-    public class MessageDTO {
-        String message;
     }
 
 }
